6af35c85866be1b69f7f01cb592e41d199d4602f
kinjo-kirato11 <2026opnewemp005@fullness.email>
2026-04-27 15:37:22 +0900

    演習６

---
+++ Ex_02_06/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Ex_02_06;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("数値を入力してください->");
+        var value1 = double.Parse(Console.ReadLine());//変数valueに入力された値を代入
+        if (value1 % 4 == 0)
+        {
+            System.Console.WriteLine("4の倍数です。");
+        }
+        else
+        {
+            System.Console.WriteLine("4の倍数ではありません");
+        }
+
+    }
+}
