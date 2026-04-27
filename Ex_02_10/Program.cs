ce95e2d679f7eb05323c3cd01536ee391bd6ef40
kinjo-kirato11 <2026opnewemp005@fullness.email>
2026-04-27 16:23:30 +0900

    演習１０

---
+++ Ex_02_10/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Ex_02_10;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+
+
+
+        string[] ekis = { "品川", "大崎", "五反田", "目黒", "恵比寿", "渋谷", "原宿", "代々木", "新宿" };
+
+
+        foreach (string eki in ekis)
+        {
+            Console.WriteLine(eki);
+        }
+    }
+}
