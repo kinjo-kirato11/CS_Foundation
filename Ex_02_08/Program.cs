4772a0bfebef93242b852e5f26008769e0f19da7
kinjo-kirato11 <2026opnewemp005@fullness.email>
2026-04-27 16:06:04 +0900

    演習８

---
+++ Ex_02_08/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Ex_02_06;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+
+
+
+        string[] eki = { "品川", "大崎", "五反田", "目黒", "恵比寿", "渋谷", "原宿", "代々木", "新宿" };
+
+        int i = 0;
+        while (i < eki.Length)
+        {
+            System.Console.WriteLine(eki[i]); // 配列の要素を出力
+            i++; // インデックスをインクリメント
+        }
+    }
+}
