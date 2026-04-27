a187191c2b4aa927300dc76c0ac9aa224298164a
kinjo-kirato11 <2026opnewemp005@fullness.email>
2026-04-27 15:48:48 +0900

    演習７

---
+++ Ex_02_07/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Ex_02_06;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("数値を入力してください->");
+        var value1 = double.Parse(Console.ReadLine());//変数valueに入力された値を代入
+        if (value1  > 0)
+        {
+            System.Console.WriteLine("正の値です。");
+        }
+        else if(value1 < 0)
+        {
+            System.Console.WriteLine("負の値です。");
+        }
+        else
+        {
+            System.Console.WriteLine("０です。");
+        }
+
+    }
+}
