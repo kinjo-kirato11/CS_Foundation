0bd9e63750a6f092817ecf065f7e7e1e50e31737
kinjo-kirato11 <2026opnewemp005@fullness.email>
2026-04-27 14:55:17 +0900

    演習５

---
+++ Ex_02_05/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Ex_02_05;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("数値を入力してください->");
+        var value1 = double.Parse(Console.ReadLine());//変数valueに入力された値を代入
+        if (value1 % 2 == 0)
+        {
+            value1 = value1*10;
+        }
+        System.Console.WriteLine("結果:" + value1);
+
+
+
+
+    }
+}
