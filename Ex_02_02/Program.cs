56851b3573324d8ae1d2ff9f53ac1e1d07d2ccb0
kinjo-kirato11 <2026opnewemp005@fullness.email>
2026-04-27 13:12:04 +0900

    演習２

--- Ex_02_02/Program.cs
+++ Ex_02_02/Program.cs
@@ -6,7 +6,7 @@ class Program
     {
         Console.Write("文字列を入力してください->");
         string value = Console.ReadLine();//変数valueに入力された値を代入
-        Console.WriteLine(  ); //入力された値を表示
+        Console.WriteLine(value); //入力された値を表示
     }
 }
 
