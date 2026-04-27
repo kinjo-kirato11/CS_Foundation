b9226adf093147c735685c4fcb2d546c620a2a42
kinjo-kirato11 <2026opnewemp005@fullness.email>
2026-04-27 16:50:17 +0900

    演習１２

---
+++ Ex_02_12/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Ex_02_12;
+
+static class Program
+{
+    static int Mul3(int a)
+    {
+        return a * 3; // 引数aを３倍にして返す
+    }
+    static void Main(string[] args)
+    {
+        Console.Write("値を入力してください: ");
+        int num1 = int.Parse(Console.ReadLine());
+        int result =Mul3(num1);
+        Console.WriteLine(result);
+    }
+}
