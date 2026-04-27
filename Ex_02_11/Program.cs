2131280e262d102f4a0448d9302419299c4b2649
kinjo-kirato11 <2026opnewemp005@fullness.email>
2026-04-27 16:35:18 +0900

    演習１１

---
+++ Ex_02_11/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Ex_02_10;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("駅名を入力してください");
+        string str = Console.ReadLine();
+        string[] ekis = { "品川", "大崎", "五反田", "目黒", "恵比寿", "渋谷", "原宿", "代々木", "新宿" };
+
+        foreach (string eki in ekis)
+        {
+            if (str == eki)
+            {
+                continue;
+            }
+            Console.WriteLine(eki);
+        }
+    }
+}
