d1f40fe96648d972111c041ed17a9bac0b92180a
kinjo-kirato11 <2026opnewemp005@fullness.email>
2026-04-27 14:23:50 +0900

    演習３

---
+++ Ex_02_03/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Ex_02_03;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("数値を入力してください->");
+        var value = double.Parse(Console.ReadLine());//変数valueに入力された値を代入
+        Console.WriteLine(value); //入力された値を表示
+        int a = 5;
+        value += a;
+
+        System.Console.WriteLine("5を加算した結果は" + value+"です");
+
+        value = value-a-a;
+        System.Console.WriteLine("5を減算した結果は" + value+"です");
+
+
+    }
+}
