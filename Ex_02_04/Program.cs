8d3682eaf1547ef169ac0b31d5ee22202dc5cf8a
kinjo-kirato11 <2026opnewemp005@fullness.email>
2026-04-27 14:46:04 +0900

    演習４

---
+++ Ex_02_04/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Ex_02_04;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("数値1を入力してください->");
+        var value1 = double.Parse(Console.ReadLine());//変数valueに入力された値を代入
+        Console.Write("数値2を入力してください->");
+        var value2 = double.Parse(Console.ReadLine());//変数valueに入力された値を代入
+        Console.Write("数値3を入力してください->");
+        var value3 = double.Parse(Console.ReadLine());//変数valueに入力された値を代入
+
+        System.Console.WriteLine("入力結果");
+
+        System.Console.WriteLine("数値1:" + value1);
+        System.Console.WriteLine("数値2:" + value2);
+        System.Console.WriteLine("数値3:" + value3);
+
+
+
+
+    }
+}
