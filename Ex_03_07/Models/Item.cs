f75238b1c8d89f84c4afaa2a8926743b55548ad5
kinjo-kirato11 <2026opnewemp005@fullness.email>
2026-04-28 16:36:58 +0900

    演習3-7

---
+++ Ex_03_07/Models/Item.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ex_03_07.Models
+{
+    public class Item
+    {
+            
+        public int ItemNo { get; set; }
+        public string ItemName { get; set; }
+        public int Price { get; set; }
+        public void Print()
+    {
+        Console.WriteLine("結果");
+        Console.WriteLine("商品番号=" + ItemNo);
+        Console.WriteLine("商品名=" + ItemName);
+        Console.WriteLine("単価=" + Price);
+        //Console.Write("税込み金額=" + sprice);
+
+
+    }
+       public Item(int ItemNo, string ItemName, int pprice)
+    {
+        this.ItemNo = ItemNo;
+        this.ItemName = ItemName;
+        this.Price = pprice;
+    }
+    }
+}
---
+++ Ex_03_07/Program.cs
@@ -0,0 +1,19 @@
+﻿using Ex_03_07.Models;
+namespace Ex_03_07;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("商品番号を入力してください→");
+        int ItemNo = int.Parse(Console.ReadLine());
+        Console.Write("商品名を入力してください→");
+        string ItemName = Console.ReadLine();
+        Console.Write("単価を入力してください→");
+        int Price = int.Parse(Console.ReadLine());
+        Item item = new Item(ItemNo, ItemName, Price);
+        // item.CalculateTaxAmount(price);
+
+        item.Print();
+    }
+}
