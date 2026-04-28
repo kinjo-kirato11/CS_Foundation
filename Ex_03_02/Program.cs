81410b46cfae1f8137e7f6438171e05ea3cc63fd
kinjo-kirato11 <2026opnewemp005@fullness.email>
2026-04-28 11:04:42 +0900

    演習3-2

---
+++ Ex_03_02/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Ex_03_02;
+
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        Item item = new Item();
+
+        Console.Write("商品番号を入力してください→");
+        item.num = int.Parse(Console.ReadLine());
+        Console.Write("商品名を入力してください→");
+        item.name = Console.ReadLine();
+        Console.Write("単価を入力してください→");
+        item.price = int.Parse(Console.ReadLine());
+
+        item.Print();
+
+    }
+}
---
+++ Ex_03_02/item.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ex_03_02;
+/// <summary>
+/// 商品情報を表すクラス
+/// </summary>
+class Item
+{
+    public int num { get; set; }
+    public string name { get; set; }
+    public int price { get; set; }
+
+public void Print(){
+    Console.WriteLine("結果");
+    Console.WriteLine("商品番号=" + num);
+    Console.WriteLine("商品名=" + name);
+    Console.WriteLine("単価=" + price);
+}
+    
+}
