2d7b0ede0ebcfc8652be9a4759439f345fec8644
kinjo-kirato11 <2026opnewemp005@fullness.email>
2026-04-30 14:57:55 +0900

    演習3-13

---
+++ Ex_03_13/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Cryptography;
+using Ex_03_13;
+
+namespace Ex_03_13;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        int r = 5;
+        Circle circle = new Circle(r);
+
+        Console.WriteLine($"{r}*{r}*{Circle.PI}={Circle.AreaCalculation(r)}");
+        Console.WriteLine($"{r}*{r}*{Circle.PI}={circle.AreaCalculation()}");
+
+        Circle.PI= 3;
+        Console.WriteLine($"{r}*{r}*{Circle.PI}={Circle.AreaCalculation(r)}");
+        Console.WriteLine($"{r}*{r}*{Circle.PI}={circle.AreaCalculation()}");
+
+    }
+}
