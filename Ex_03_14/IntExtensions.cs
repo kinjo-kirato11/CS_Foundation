05aed92e1539e5554ad8231db5fa857c5d3252b2
kinjo-kirato11 <2026opnewemp005@fullness.email>
2026-04-30 15:10:43 +0900

    演習3-14

---
+++ Ex_03_14/IntExtensions.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ex_03_14
+{
+    public static class IntExtensions
+    {
+        public static int Add(this int a, int b)
+        {
+            return a+b;
+        }
+        public static double Average(this int a, int b)
+        {
+            return (a+b)/2;
+        }
+    }
+}
---
+++ Ex_03_14/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Ex_03_14;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("値1を入力してください->");
+        int a = int.Parse(Console.ReadLine());
+        Console.Write("値2を入力してください->");
+        int b = int.Parse(Console.ReadLine());
+        Console.WriteLine("加算:" + a.Add(b));
+        Console.WriteLine("平均:" + a.Average(b));
+    }
+}
