81580257e1617cf2c1459844951da28a4f9bb433
kinjo-kirato11 <2026opnewemp005@fullness.email>
2026-04-30 15:24:48 +0900

    演習3-15

---
+++ Ex_03_15/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Ex_03_15;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        Calculator calc = new Calculator();
+        try{
+        Console.Write("値1を入力してください->");
+        int a = int.Parse(Console.ReadLine());
+        Console.Write("値2を入力してください->");
+        int b = int.Parse(Console.ReadLine());
+        Console.WriteLine("値1 / 値2 = " + calc.Div(a,b));
+        }
+        catch(DivideByZeroException ex)
+        {
+            Console.WriteLine("例外:"+ ex.Message);
+        }
+    }
+}
