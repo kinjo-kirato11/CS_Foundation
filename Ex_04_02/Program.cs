45cff7232e8ef0bed4a6b37019869a3676a07dc0
kinjo-kirato11 <2026opnewemp005@fullness.email>
2026-05-01 08:38:26 +0900

    演習4-2

---
+++ Ex_04_02/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Ex_04_02;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("年を入力してください→");
+        int year = int.Parse(Console.ReadLine());
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+        {
+            Console.WriteLine($"{year}年は閏年です。");
+
+        }
+        else
+        {
+        Console.WriteLine($"{year}年は閏年ではありません。");
+
+        }
+    }
+}
